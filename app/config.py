9f1aad02c85e194239c36c2fdb5994b74a8f6532
timileyin42 <akanmuibro@gmail.com>
2025-01-22 10:55:08 +0000

    log table added

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key_here")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "your_jwt_secret_key_here")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
